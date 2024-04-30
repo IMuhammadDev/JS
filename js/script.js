b2c9cd4627e24776160409aa2d99763079f5cf3b
ikrom <imuhammad1507@gmail.com>
2024-04-30 20:22:11 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 "use strict";
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+const seriesDB = {
+  count: numberOfSeries,
+  series: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
 
-const mmm = 6 +"9"
-const mmmm = "6" + 6
-console.log(typeof mmmm)
+let i = 0;
+while (i < 2) {
+  const a = prompt("Oxirgi ko'rgan serialingizni kiriting", "");
+  const b = prompt("Necha baxo berasiz", "");
+  if (a != null && b != null && a != "" && b != "") {
+    seriesDB.series[a] = b;
+    console.log("done");
+    i++;
+  } else {
+    console.log(error);
+    i--;
+  }
+}
+
+if (seriesDB.count < 5) {
+  console.log("siz kam serial ko'ribsiz");
+} else if (seriesDB.count > 5 && seriesDB.count < 10) {
+  console.log("siz classik tomoshabin ekansiz");
+} else if (seriesDB.count > 10) {
+  console.log("siz serialchi zvezda ekansiz");
+}
+
+console.log(seriesDB);
