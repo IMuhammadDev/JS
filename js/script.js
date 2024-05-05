ba6c487920b5582717bf92cb6ae6c0bd4694c7ab
ikrom <imuhammad1507@gmail.com>
2024-05-05 11:17:19 +0500

    the next

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 "use strict";
-const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+let numberOfSeries;
+function strartApp() {
+  numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+
+  while (
+    numberOfSeries == null ||
+    numberOfSeries == "" ||
+    isNaN(numberOfSeries)
+  ) {
+    numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+  }
+}
+strartApp();
+
 const seriesDB = {
   count: numberOfSeries,
   series: {},
@@ -8,26 +21,50 @@ const seriesDB = {
   private: false,
 };
 
-let i = 0;
-while (i < 2) {
-  const a = prompt("Oxirgi ko'rgan serialingizni kiriting", "");
-  const b = prompt("Necha baxo berasiz", "");
-  if (a != null && b != null && a != "" && b != "") {
-    seriesDB.series[a] = b;
-    console.log("done");
-    i++;
-  } else {
-    console.log(error);
-    i--;
+function rememberOfSerias() {
+  let i = 0;
+  while (i < 2) {
+    const a = prompt("Oxirgi ko'rgan serialingizni kiriting", "");
+    const b = prompt("Necha baxo berasiz", "");
+    if (a != null && b != null && a != "" && b != "") {
+      seriesDB.series[a] = b;
+      console.log("done");
+      i++;
+    } else {
+      console.log(error);
+      i--;
+    }
+  }
+}
+rememberOfSerias();
+
+function checkStatusUser() {
+  if (seriesDB.count < 5) {
+    console.log("siz kam serial ko'ribsiz");
+  } else if (seriesDB.count > 5 && seriesDB.count < 10) {
+    console.log("siz classik tomoshabin ekansiz");
+  } else if (seriesDB.count > 10) {
+    console.log("siz serialchi zvezda ekansiz");
+  }
+}
+checkStatusUser();
+
+function writeGenres() {
+  for (let i = 0; i < 3; i++) {
+    let gener = prompt(`yaxshi ko'rgan janringiz ${i+1} ?`,"")
+    seriesDB.genres[i]=gener
   }
 }
 
-if (seriesDB.count < 5) {
-  console.log("siz kam serial ko'ribsiz");
-} else if (seriesDB.count > 5 && seriesDB.count < 10) {
-  console.log("siz classik tomoshabin ekansiz");
-} else if (seriesDB.count > 10) {
-  console.log("siz serialchi zvezda ekansiz");
-}
+writeGenres()
 
-console.log(seriesDB);
+function showDb() {
+  if (seriesDB.private == false) {
+    console.log(seriesDB);
+  } else {
+    console.log("error");
+  }
+}
+showDb();
+
+
